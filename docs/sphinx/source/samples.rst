ce36600523963094d278dbe6bdfe7d1700999bda
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-21 15:34:38 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * [ ] http://www.apache.org/licenses/ with 4 occurrences migrated to:
      https://www.apache.org/licenses/ ([https](https://www.apache.org/licenses/) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE-2.0 with 112 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).

--- docs/sphinx/source/samples.rst
+++ docs/sphinx/source/samples.rst
@@ -122,7 +122,7 @@ NOTE: This whole script is contained in one file, and marked up as::
      you may not use this file except in compliance with the License.
      You may obtain a copy of the License at
 
-         http://www.apache.org/licenses/LICENSE-2.0
+         https://www.apache.org/licenses/LICENSE-2.0
 
      Unless required by applicable law or agreed to in writing, software
      distributed under the License is distributed on an "AS IS" BASIS,
--- samples/setup-template.py
+++ samples/setup-template.py
@@ -6,7 +6,7 @@
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+       https://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,7 +31,7 @@ setup(name='springpython-samples',
       author='Greg L. Turnquist',
       author_email='gregturn at mindspring dot com',
       url='http://springpython.webfactional.com',
-      license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
+      license='Apache Software License (https://www.apache.org/licenses/LICENSE-2.0)',
       packages=['petclinic', 
                 'petclinic.cherrypy',
                 'petclinic.db',
